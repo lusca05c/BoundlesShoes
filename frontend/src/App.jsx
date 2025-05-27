fffc5ab52914c80618f3f97d4de52fa2f32d8eaa
lusca05c <lucasallan.contato@gmail.com>
2025-05-27 09:18:42 -0300

    feat: adicionando novamente o App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter} from "react-router"
+import { Router } from '../router'
+
+export function App() {
+
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
+}
