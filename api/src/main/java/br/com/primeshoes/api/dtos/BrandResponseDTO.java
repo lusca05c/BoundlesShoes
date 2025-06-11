877a6aaf8410249b5d09302ca17cac47264d9293
lusca05c <lucasallan.contato@gmail.com>
2025-06-10 22:53:15 -0300

    feat: adicionando os DTOs de Brand

---
+++ api/src/main/java/br/com/primeshoes/api/dtos/BrandResponseDTO.java
@@ -0,0 +1,5 @@
+package br.com.primeshoes.api.dtos;
+
+public record BrandResponseDTO (long id, String name) {
+
+}
---
+++ api/src/main/java/br/com/primeshoes/api/dtos/BrandUpdateDTO.java
@@ -0,0 +1,5 @@
+package br.com.primeshoes.api.dtos;
+
+public record BrandUpdateDTO (long id, String name, long productId) {
+
+}
