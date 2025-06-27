bdd9cfc9dd51db3f865154de4132a9a39388d8e5
lusca05c <lucasallan.contato@gmail.com>
2025-06-27 13:26:02 -0300

    refactor: modificações em builds de padronização e componentes de Home

---
+++ api/src/main/java/br/com/primeshoes/api/entites/Cart.java
@@ -0,0 +1,23 @@
+package br.com.primeshoes.api.entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.OneToOne;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+	
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@OneToOne
+    @JoinColumn(name = "users_id")
+	private User user;
+
+}
--- frontend/src/components/NavLeftBar.jsx
+++
@@ -1,9 +0,0 @@
-export function NavLeftBar(){
-    return(
-        <>
-            <div className="bg-[#D9D9D9] min-h-screen w-60">
-                GORDURA TRANS
-            </div>
-        </>
-    )
-}
