f6e0c83cd1ab79b60d18738b49d0359ea62a2b5c
lusca05c <lucasallan.contato@gmail.com>
2025-07-04 08:43:55 -0300

    feat: adicionando os DTOs de Image

---
+++ api/src/main/java/br/com/primeshoes/api/dtos/ImageCreateDTO.java
@@ -0,0 +1,5 @@
+package br.com.primeshoes.api.dtos;
+
+public record ImageCreateDTO (String filePath){
+    
+}
---
+++ api/src/main/java/br/com/primeshoes/api/dtos/ImageUpdateResponseDTO.java
@@ -0,0 +1,5 @@
+package br.com.primeshoes.api.dtos;
+
+public record ImageUpdateResponseDTO(long id, String filePath) {
+    
+}
