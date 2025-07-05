1a4bcd7f136741be5b17549039d5462b0851838b
lusca05c <lucasallan.contato@gmail.com>
2025-07-04 23:37:44 -0300

    Adicionando modificações no index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,12 +2,14 @@ import  { Home } from "./pages/home"
 import { Login } from "./pages/login"
 import { PasswordRecadastro } from "./pages/passwordRecadastro" 
 import { UserCadastro } from "./pages/userCadastro" 
-import { UserPerfil } from "./pages/userPerfil" 
+import { UserPerfil } from "./pages/userPerfil"
+import { RecuperarSenha } from "./pages/recuperarSenha"
 
 export const Pages = {
     Home, 
     Login, 
     UserPerfil, 
     UserCadastro, 
-    PasswordRecadastro 
+    PasswordRecadastro,
+    RecuperarSenha
 }
